888294545f48d2a3d88a57f49ec1dd49c3033afd
Phumsirii <phumsirisumativit@gmail.com>
2025-11-18 11:15:57 +0700

    add get user grade function

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,5 +1,6 @@
 from fastmcp import FastMCP
 from fastmcp.server.dependencies import get_access_token
+import httpx
 
 async def get_me() -> dict:
     """Get the user's information."""
@@ -9,3 +10,15 @@ async def get_me() -> dict:
         "user": token.claims,
     }
 
+async def get_user_gradeletter(courseId: str):
+    token = get_access_token()
+    access_token = token.token
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    url = f"https://www.mycourseville.com/api/v1/public/get/user/gradeletter?cv_cid={courseId}"
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, headers=headers)
+        resp.raise_for_status()
+        gradeletter = resp.json()
+        return gradeletter
